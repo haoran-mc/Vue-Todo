53da4078f067ab42fe6daf95ada0feb9adadf644
haoran <haoran.mc@outlook.com>
2021-08-30 23:08:26 +0800

    :beers:

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,28 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Container,
+  Header,
+  Main,
+  Row,
+  Col,
+  Divider,
+  Footer,
+  Input,
+  Card,
+  Table,
+  TableColumn
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Divider)
+Vue.use(Footer)
+Vue.use(Input)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    hello
-  </div>
+  <el-container class="todo-container">
+    <!-- 头部区域 -->
+    <el-header>Todo清单</el-header>
+    <!-- 页面主体区域 -->
+    <el-main>
+      <el-row>
+        <el-col>
+          <TodoList></TodoList>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>Gin框架小练习</el-footer>
+  </el-container>
 </template>
 
 <script>
+  // @ is an alias to /src
+  import TodoList from '@/components/TodoList.vue'
+
   export default {
+    components: {
+      TodoList
+    }
   }
 </script>
 
 <style>
+  .todo-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #8b8b8b;
+    color: #fff;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-footer {
+    background-color: #8b8b8b;
+    color: #fff;
+    text-align: right;
+    line-height: 60px;
+  }
 </style>
